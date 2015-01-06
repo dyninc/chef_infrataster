14941ba5d09bb5a4cc7ce0e993dd27ff7e73f684
Paul Thomas <paul+github@paulthomas.eu>
2015-01-06 12:59:38 +0000

    tidy quotes

--- spec/infrataster/default_spec.rb
+++ spec/infrataster/default_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 require 'pp'
 
 describe server('provisiontest') do
-  describe http("http://192.168.20.10/") do
-    it "responds with 403 'forbidden'" do
+  describe http('http://192.168.20.10/') do
+    it 'responds with 403 forbidden' do
       expect(response.status).to eq 403
     end
   end
